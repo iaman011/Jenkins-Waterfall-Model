fdaea0b6ee86faf2a32ac4bfe24059c87f280c9c
Aman Singh <iaman.singh011@gmail.com>
2025-07-23 16:30:37 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,15 @@
+print("Hello! This is a Test Stage")
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error"
+    return a / b
