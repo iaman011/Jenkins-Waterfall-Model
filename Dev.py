542d2db09b56fef349bb5526ffb80809a501f13e
Aman Singh <iaman.singh011@gmail.com>
2025-07-23 16:24:49 +0530

    Create Dev.py

---
+++ Dev.py
@@ -0,0 +1,13 @@
+print("Hello! World, This is a Dev stage")
+a = 10
+b = 20
+
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b if b != 0 else "Error: Division by zero"
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
